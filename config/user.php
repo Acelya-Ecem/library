06c8b937abe012e394ee46bf9d1ebd1413d0a41f
acelyaecem <acelya.ecem.oksuz@Icloud.com>
2022-08-10 21:01:24 +0300

    Created user login module.

--- config/user.php
+++ config/user.php
@@ -10,34 +10,61 @@
         public function __construct() //nesne ilk tanımlandığında bu fonksiyon çalışacak. "!" tersi olursa anlamına gelir.
         {
             session_start();
-            if(!isset($_SESSION['userName'])){ // set edilmediyse yok et
+
+            /* if(!isset($_SESSION['userName'])){ // set edilmediyse yok et
                 session_destroy();
+            } */
+        }
+
+        public function login($userName, $passW){
+            require("db.php");
+
+            $query = $db->prepare("SELECT * FROM users Where userName = ? and passW = ?"); //böyle yaparsak daha güvenli
+            $query->execute(array($userName, $passW));
+
+            $users = $query->fetchAll(PDO::FETCH_OBJ); //obje olarak parçalanıyor
+            $passW = null; //şifreye gerek kalmdaı
+
+            if($query->rowCount() == 1){ //tam anlamadım ?? ama peki
+                //session_start(); gereksiz olduğu için koddan çıkarıldı
+
+                foreach($users as $u){ //tam anlamadım ?? ama peki (galiba veri tabnanından çekip classa aktarıyor)
+                    $this->userID = $u->userID; //this --> $u ??
+                    $this->userName = $u->userName;
+                    $this->name = $u->name;
+                    $this->surName = $u->surName;
+                    $this->RGTime = $u->registertime;
+                }
+                $_SESSION['userID'] = $this ->userID;
+                $_SESSION['userName'] = $this ->userName;
+                $_SESSION['name'] = $this ->name;
+                $_SESSION['RGTime'] = $this ->RGTime;
+
+                header("Refresh:0 ; url=index.php");
+            }
+
+            else{
+                $message = "User not found or wrong password!";
+                echo  "
+                    <script type = 'text/javascript'>
+                        alert('$message');
+                    </script>
+                ";
+                $this->logout();
             }
         }
 
-        public function login($userName, $passW) {
-            $this->userName = $userName; //this zamir classı ifade eder. (CAlssın zamiri)
-            $this->passW = $passW;
+        public function logout(){
+            session_unset();
+            session_destroy();
+            header("Refresh:0 ; url=index.html"); //kullanıcıyı sayfalar arasında yönlendirir
+            exit();
+        }
 
-            require_once "db.php";
-
-            $query = $db->prepare("SELECT * FROM users WHERE
-             userName = ? AND passW = ?
-             '$userName'");//query hazırlığı yapar. (???? - Burada hangi userName i kullanacağımızı seçer)
-
-             $query->execute(array($this->userName, $this->passW)); // (?) - ? lerini doldurur (look ne işe yarıyor????)
-
-             if($query->rowCount()) // ya eşleşiyor ya eşleşmiyor. Aynı isme ve password a sahip iki kişi yok. username ve password yan yana yazılıyor.
-             {
-                session_start();
-                foreach($query as $row)
-                {
-                    $_SESSION['userID'] = $row['userID'];
-                    $_SESSION['userName'] = $row['userName'];
-                    $_SESSION['name'] = $row['name'];
-                    $_SESSION['RGTime'] = $row['RGTime'];
-                }
-             }
+        public function isSession(){
+            if(!isset($_SESSION['userName']) && !isset($_SESSION['userID'])){ //session username VE userID tanımlanMAmışsa (tam işlevi benim açımdan sorgulanabilir.)
+                $this->logout();
+            }
         }
     }
 ?>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    require("config/user.php");
+
+    $u = new user();
+    $u->isSession();
+
+    echo $_SESSION['userName'];
+?>
